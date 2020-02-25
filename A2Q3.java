6f27e911e1b57c71a20511e884e4d806598eb16d
NichHarris <harris.nicholas1998@gmail.com>
2020-02-24 19:23:08 -0500

    Add code for question 3

--- A2Q3.java
+++ A2Q3.java
@@ -9,49 +9,49 @@ import java.util.Random;
 
 public class A2Q3 {
 
-    public static Integer[] mergeSort(Integer[] arr, Integer[] perm, int lo, int mid, int hi){
-        Integer[] temp = new Integer[arr.length];
-        int x = lo;
-        int y = mid + 1;
-        for(int i = lo; i <= hi; i++){
-            temp[i] = arr[i];
+    public static void mergeSort(Integer[] arr, Integer[] perm, Integer[] temp, int lo, int mid, int hi){
+        // Integer[] temp = new Integer[arr.length];
+        int i = lo;
+        int j = mid + 1;
+        for(int k = lo; k <= hi; k++){
+            temp[k] = perm[k];
         }
 
-        for(int j = lo; j < hi; j++){
-            if(x > mid){
-                perm[j] = temp[y++];
+        for(int k = lo; k <= hi; k++){
+            if(i > mid){
+                perm[k] = temp[j++];
             }
-            else if(y > hi){
-                perm[j] = temp[x++];
+            else if(j > hi){
+                perm[k] = temp[i++];
             }
-            else if(temp[y] < temp[x]){
-                perm[j] = temp[y++];
+            else if(arr[temp[j]] < arr[temp[i]]){
+                perm[k] = temp[j++];
             }
             else{
-                perm[j] = temp[x++];
+                perm[k] = temp[i++];
             }
         }
+    }
+
+    public static Integer[] sort(Integer[] arr){
+        Integer[] perm = new Integer[arr.length];
+        Integer[] temp = new Integer[arr.length];
+        for(int i = 0; i < arr.length; i++){
+            perm[i] = i;
+        }
+        indirectSort(arr, perm, temp, 0, arr.length - 1);
         return perm;
     }
 
-    public static void sort(Integer[] arr, Integer[] perm, int lo, int hi){
-        if(lo < hi){
-            int mid = (lo+hi)/2;
-            sort(arr, perm, lo, mid);
-            sort(arr, perm, mid+1,hi);
-            perm = mergeSort(arr, perm, lo, mid, hi);
+    public static void indirectSort(Integer[] arr, Integer[] perm, Integer[] temp, int lo, int hi){
+        if(hi <= lo){
+            return;
         }
+        int mid = lo + (hi - lo)/2;
+        indirectSort(arr, perm, temp, lo, mid);
+        indirectSort(arr, perm, temp, mid + 1, hi);
+        mergeSort(arr, perm, temp, lo, mid, hi);
     }
-
-    // public static void indirectSort(Integer[] arr, Integer[] perm, int lo, int hi){
-    //     if(lo < hi){
-    //         return;
-    //     }
-    //     int mid = lo + (hi - lo) / 2;
-    //     indirectSort(arr, perm, lo, mid);
-    //     indirectSort(arr,perm, mid + 1, hi);
-    //     perm = mergeSort(arr, perm, lo, mid, hi);
-    // }
   
     public static void print(Integer[] arr){
         for(int i = 0; i < arr.length; i++){
@@ -62,14 +62,15 @@ public class A2Q3 {
     public static void main(String[] args){
         int size = 20;
         Integer[] array = new Integer[size];
-        Integer[] perm = new Integer[size];
+        Integer[] newArray = new Integer[size];
         Random rand = new Random();
         for (int i = 0; i < size; i++){
             array[i] = rand.nextInt(1000);
         }  
+        System.out.print("Orig array: ");
         print(array);
-        // indirectSort(array, perm, 0, size-1);
-
-        print(perm);
+        newArray = sort(array);
+        System.out.print("perm array: ");
+        print(newArray);
     }
 }
