f30aa81b0274c4c660c51508d4379704f3bc07d0
NichHarris <harris.nicholas1998@gmail.com>
2020-03-17 20:18:16 -0400

    Fix question 2, start question 4

--- Assignment 3/Question 2/ClientFile.java
+++ Assignment 3/Question 2/ClientFile.java
@@ -5,14 +5,12 @@
 shown below. (You need to create a file with the format of the input to test your program.) */
 
 import java.lang.FileData;
-import java.io.IOException;
-import java.io.FileReader;
-import java.io.BufferedReader;
+import java.io.*;
 
 public class ClientFile{
     public static void sortByVolume(FileData[] data, int size){
         for(int i = 1; i < size; i++){
-            int key = data[i].volume;
+            long key = data[i].volume;
             int j = i - 1;
         
             while(j >= 0 && data[j].volume > key){
@@ -30,7 +28,7 @@ public class ClientFile{
         int count = 0;
         while((line = buffer.readLine())!= null){
             String date = line.split(" ")[0];
-            long volume = Long.parseLong(line.split("")[1]);
+            long volume = Long.parseLong(line.split(" ")[1]);
             data[count++] = new FileData(date, volume);
         }
         buffer.close();
@@ -38,8 +36,8 @@ public class ClientFile{
     }
 
     public static void outputData(FileData[] data, String file, int size) throws IOException{
-        FileReader reader = new FileReader(file);
-        BufferedReader buffer = new BufferedReader(reader);
+        FileWriter writer = new FileWriter(file);
+        BufferedWriter buffer = new BufferedWriter(writer);
         for(int i = 0; i < size; i++){
             buffer.write(data[i].toString());
         }
--- Assignment 3/Question 2/FileData.java
+++ Assignment 3/Question 2/FileData.java
@@ -4,7 +4,7 @@
 /* 2.5.9 Develop a data type that allows you to write a client that can sort a file such as the one
 shown below. (You need to create a file with the format of the input to test your program.) */
 
-class FileData implements Comparanble<FileData>{
+class FileData {
     String date;
     long volume;
 
