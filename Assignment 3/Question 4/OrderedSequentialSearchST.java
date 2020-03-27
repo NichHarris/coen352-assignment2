a73c00233a12298cbae2cc3402b7228e2467b9ec
NichHarris <harris.nicholas1998@gmail.com>
2020-03-27 09:47:39 -0400

    Add question for Q4

--- Assignment 3/Question 4/OrderedSequentialSearchST.java
+++ Assignment 3/Question 4/OrderedSequentialSearchST.java
@@ -1,3 +1,9 @@
+// Nicholas Harris
+// 40111093
+
+/* 3.1.3 Develop a symbol-table implementation OrderedSequentialSearchST that uses an
+ordered linked list as the underlying data structure to implement our ordered symbol-table APL */
+
 import java.util.*;
 
 /* public class ST<Key extends Comparable<Key>, Value>
